9c34188e462f68b68f54d90fec97cfa80a54f9d8
PvtPrinny <tsuhaohsu@gmail.com>
2024-12-01 22:47:28 +0800

    add new template for db.py

---
+++ fastapi/db.template.py
@@ -0,0 +1,11 @@
+# from motor.motor_asyncio import AsyncIOMotorClient
+# from pydantic_settings import BaseSettings
+
+
+# class Settings(BaseSettings):
+#     mongo_uri: str = "mongodb://192.168.0.10:27017" // your mongodb ip
+
+# settings = Settings()
+
+# client = AsyncIOMotorClient(settings.mongo_uri)
+# database = client["Echo_Text_Local"]
